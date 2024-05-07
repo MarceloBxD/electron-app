04fd3b5aef3faabb568c3065e749764182b9f64d
Marcelo Bracet <marcelo.bracet1@gmail.com>
2024-05-07 00:49:40 -0300

    feat: Add tray icon and context menu to main.js

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
-const { app, Tray } = require("electron");
+const { app, Tray, Menu } = require("electron");
+const { resolve } = require("path");
+
+console.log();
 
 let tray = null;
 
-app.whenReady().then(() => {
-  tray = new Tray("/assets/icon.png");
-});
+app
+  .whenReady()
+  .then(() => {
+    tray = new Tray(
+      resolve(__dirname, "assets/images", "trayiconTemplate.png")
+    );
+
+    const contextMenu = Menu.buildFromTemplate([
+      {
+        label: "Fiat Titano",
+        type: "radio",
+      },
+      {
+        label: "Jeep Compass 4XE",
+        type: "radio",
+      },
+      {
+        label: "Fiat Argo",
+        type: "radio",
+      },
+    ]);
+
+    tray.setToolTip("Main Folders - VS Code");
+    tray.setContextMenu(contextMenu);
+  })
+  .catch((err) => {
+    console.log("Error creating tray", err);
+  });
